4f2746500efe7f66335b3bb438d95f76dfbaf146
ManganiBiagio <biagiomangani@live.it>
2023-01-20 18:02:38 +0100

    add footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,6 +1,27 @@
 <footer>
    @include('partials.boxBanner1')
    @include("partials.boxBanner2")
+   <div class="box-footer">
+    <div class="container">
+        <div class="d-flex">
+            <div class="btn-box">
+                <button class="my-btn">SIGN-UP NOW!</button>
+            </div>
+            <div class="link-box">
+                <div class="txt-footer">FOLLOW US</div>
+                <div><img src="{{Vite::asset('resources/img/footer-facebook.png')}}" alt=""></div>
+                <div><img src="{{Vite::asset('resources/img/footer-twitter.png')}}" alt=""></div>
+                <div><img src="{{Vite::asset('resources/img/footer-youtube.png')}}" alt=""></div>
+                <div><img src="{{Vite::asset('resources/img/footer-pinterest.png')}}" alt=""></div>
+                <div><img src="{{Vite::asset('resources/img/footer-periscope.png')}}" alt=""></div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+   </div>
 
 
 
