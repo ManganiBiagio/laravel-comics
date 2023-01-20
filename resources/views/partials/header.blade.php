47a1b11621fe7c0f4457d463cbde2532779f581f
ManganiBiagio <biagiomangani@live.it>
2023-01-20 17:30:39 +0100

    add jumbotrone

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -11,7 +11,13 @@
                 
             @endforeach
         </ul>
+        <form  role="search">
+            <input class="form-control " type="search" placeholder="Search" aria-label="Search">
+           
+          </form>
         
 
     </nav>
+
 </div>
+@include('partials.jumbotrone')
---
+++ resources/views/partials/jumbotrone.blade.php
@@ -0,0 +1,5 @@
+<section class="jumbotron">
+    <div class="img-box">
+        <img src="{{Vite::asset('resources/img/jumbotron.jpg')}}" class="img-fluid" alt="">
+    </div>
+</section>
